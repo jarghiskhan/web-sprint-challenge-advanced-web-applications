f8b49a875cd7c6b84af35b5ba8f378f2419f41fa
jarghiskhan <jared.l.wyatt@outlook.com>
2021-09-12 17:55:55 -0400

    tests in progress

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import MutationObserver from 'mutationobserver-shim';
 
-import { render, screen} from "@testing-library/react";
+import { render, screen, waitFor} from "@testing-library/react";
 import BubblePage from './BubblePage';
+import {fetchColorService as mockFetch} from '../services/fetchColorService'
 
-test("Renders without errors", ()=> {
-    
+jest.mock("../services/fetchColorService");
+test("Renders without errors", async ()=> {
+    mockFetch.mockResolvedValueOnce({data:[]});
+    render(<BubblePage/>);
+    await waitFor(()=>{
+        const list = screen.getByRole('list');
+        expect(list).toHaveTextContent('colors');
+    })
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
